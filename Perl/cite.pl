8404c0703b4fc10b0e7ed3a5660aa2d0df5cbb6d
Kodi Arfer <git@arfer.net>
2014-05-23 19:09:00 -0400

    [cite.pl] Added RIS-file input.

--- Perl/cite.pl
+++ Perl/cite.pl
@@ -8,7 +8,7 @@ use Citematic::QuickBib;
 use URI::Escape;
 use Encode 'decode';
 use JSON qw(to_json);
-use File::Slurp 'write_file';
+use File::Slurp 'read_file', 'write_file';
 use Getopt::Long::Descriptive;
 
 sub runsub (&) {return $_[0]->();}
@@ -18,6 +18,7 @@ sub normalize {my $s = $_[0]; $s =~ s/\s+/ /g; $s =~ s/\A //; $s =~ s/ \z//; $s}
 my ($opt, $usage) = Getopt::Long::Descriptive::describe_options
    ('%c %o [<search-term> ...]',
     ['title|t=s@' => 'a title keyword'],
+    ['input|i=s' => 'a RIS file to parse'],
     ['bypass-ebsco-cache|b' => "don't read from the cache for EBSCOhost (useful for getting fresh full-text URLs)"],
     ['more|m' => 'include more information in the citation (breaks APA style)'],
     ['doi-url-fmt=s', 'printf format for making a URL out of a DOI',
@@ -31,43 +32,51 @@ if ($opt->help)
    {print $usage->text;
     exit;}
 
-my @title_words = map {normalize $_} @{$opt->title || []};
 local $Citematic::Get::verbose = not $opt->quiet;
 local $Citematic::Get::debug = $opt->debug;
 local $Citematic::Get::bypass_ebsco_cache = $opt->bypass_ebsco_cache;
 
-# Interpret arguments that look like years, ISBNs, DOIs, EBSCO
-# URLs, or formatted citations appropriately. Interpret the
-# remainder as author keywords.
+my $a = $opt->input
 
-my ($year, $isbn, $doi, %ebsco_record, @author_words);
+  ? Citematic::Get::digest_ris(scalar($opt->input eq '-'
+      ? do {local $/; binmode STDIN, ':utf8'; <STDIN>}
+      : read_file $opt->input, {binmode => ':utf8'}))
 
-push @author_words, map {normalize $_} grep {runsub
-   {if (/\A\d{4}\z/)
-       {$year = $_;}
-    elsif (/\bebscohost\.com\b/ and
-            m![#&]db=([a-zA-Z0-9]{3,6}).+?\bAN=([^=&#/]+)!)
-       {%ebsco_record = (db => $1, AN => uri_unescape($2));}
-    elsif (m!\A[-0-9xX]+\z!)
-       {$isbn = $_;}
-    elsif (m!\A(?:doi:)?\d+\.\d+/!)
-       {$doi = $_;}
-    elsif (/,/)
-       {!$year and s/\(?(\d+)\)?//
-            and $year = $1;
-        push @author_words, /(\w{2,})/g;}
-    else
-       {return 1;}
-    0;}}
-  @ARGV;
+  : do
+       {my @title_words = map {normalize $_} @{$opt->title || []};
 
-my $a = Citematic::Get::get
-   (author => \@author_words,
-    year => $year,
-    title => \@title_words,
-    isbn => $isbn,
-    doi => $doi,
-    ebsco_record => \%ebsco_record);
+        # Interpret arguments that look like years, ISBNs, DOIs, EBSCO
+        # URLs, or formatted citations appropriately. Interpret the
+        # remainder as author keywords.
+
+        my ($year, $isbn, $doi, %ebsco_record, @author_words);
+
+        push @author_words, map {normalize $_} grep {runsub
+           {if (/\A\d{4}\z/)
+               {$year = $_;}
+            elsif (/\bebscohost\.com\b/ and
+                    m![#&]db=([a-zA-Z0-9]{3,6}).+?\bAN=([^=&#/]+)!)
+               {%ebsco_record = (db => $1, AN => uri_unescape($2));}
+            elsif (m!\A[-0-9xX]+\z!)
+               {$isbn = $_;}
+            elsif (m!\A(?:doi:)?\d+\.\d+/!)
+               {$doi = $_;}
+            elsif (/,/)
+               {!$year and s/\(?(\d+)\)?//
+                    and $year = $1;
+                push @author_words, /(\w{2,})/g;}
+            else
+               {return 1;}
+            0;}}
+          @ARGV;
+
+        Citematic::Get::get
+           (author => \@author_words,
+            year => $year,
+            title => \@title_words,
+            isbn => $isbn,
+            doi => $doi,
+            ebsco_record => \%ebsco_record);};
 
 if ($a)
    {!$opt->quiet and $a->{DOI}
